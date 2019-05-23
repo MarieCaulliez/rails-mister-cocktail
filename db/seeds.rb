6bae0533f28c35b687fb05a01bc394135c2b3be3
Marie Caulliez <mariecaulliez@gmail.com>
2019-05-23 17:22:30 +0200

    Adds 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "Light rum")
+Ingredient.create(name: "Gin")
+Ingredient.create(name: "Champagne")
+Ingredient.create(name: "Lime juice")
+Ingredient.create(name: "Grenadine")
+Ingredient.create(name: "Pisco")
